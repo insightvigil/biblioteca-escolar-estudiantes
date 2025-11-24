71c7f34b3a81ec5d2ef1bc257cca2706b5686778
Adrian Vigil <insightvigil@gmail.com>
2025-11-24 08:50:15 -0600

    server deployed

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -1,28 +1,33 @@
-import {
-  createBrowserRouter,
-} from "react-router";
+import { createBrowserRouter } from "react-router";
 import StudentsLayout from "./layout/StudentsLayout.jsx";
 import ErrorPage from "../components/navigation/ErrorPage/ErrorPage.jsx";
 import Home from "../features/home/index.jsx";
-import BookDetailPage from "../features/book-detail/index.jsx"
+import BookDetailPage from "../features/book-detail/index.jsx";
 import CategoryBooksPage from "../features/categories/pages/CategoryBookPage/CategoryBooksPage.component.jsx";
-export const router = createBrowserRouter([
+
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <StudentsLayout />,
+      errorElement: <ErrorPage />,
+
+      children: [
+        { index: true, element: <Home /> },
+
+        // Libros
+        { path: "books/:id", element: <BookDetailPage /> },
+
+        // Categorías
+        { path: "category/:id", element: <CategoryBooksPage /> },
+
+        // (Probablemente esta ruta estaba mal formada, la dejo igual si tú lo deseas)
+        { path: "books/:bookId/category/:id", element: <CategoryBooksPage /> }
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <StudentsLayout />,     // layout padre
-    errorElement: <ErrorPage />,  // error por-ruta (404/500, etc.)
-
-    
-    children: [
-      { index: true, path:'/', element: <Home/>},
-      { path: "books/:id", element: <BookDetailPage/>},
-      {path:"category/:id", element: <CategoryBooksPage/>},
-      {path:"books/:id/category/:id", element: <CategoryBooksPage/> }
-      /*{ path: "libros", element: <BookGrid /> },
-      { path: "libros/:id", element: <BookDetail />, loader: bookLoader },
-      { path: "categorias/:catId/libros", element: <CategoryBooks />, loader: categoryLoader },
-      { path: "*", element: <NotFound /> },*/
-    ],
-  },
-]);
-
+    // Basename raíz, funciona en '/'
+    /*basename: "/"*/
+  }
+);
